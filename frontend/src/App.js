34493d7bf2624d739a34f1614d99f9c6184e5726
Paula <pauligarcia_05@hotmail.com>
2024-11-20 15:52:46 -0300

    add site styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Header from './components/layout/Header';
 import Nav from './components/layout/Nav';
 import Footer from './components/layout/Footer';
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import '../../styles/components/layout/Header.css';
 
 const Header = (props) => {
   return (
     <header>
-      <div className="holder">
+      <div className="holder header-container">
         <img src="images/logo.png" width="100" alt="Transportes X" />
         <h1>Transportes X</h1>
       </div>
