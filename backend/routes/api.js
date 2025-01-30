aae88bb73241d1ece9d9a9766d9a6a02b649200d
Paula Garcia <pauligarcia_05@hotmail.com>
2025-01-29 21:07:03 -0300

    addition of nodemailer and managed form inputs

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var newsModel = require("../models/newsModel");
 var cloudinary = require("cloudinary").v2;
+var nodemailer = require("nodemailer");
 
 router.get("/news", async function (req, res, next) {
   let news = await newsModel.getNews();
@@ -25,6 +26,27 @@ router.get("/news", async function (req, res, next) {
     }
   });
   res.json(news);
+});
+
+router.post("/contact", async (req, res) => {
+  const email = {
+    to: "pauligarcia_05@hotmail.com",
+    subject: "Web Contact",
+    html: `${req.body.name} its contacting from the web requiring more information. Please respond to ${req.body.email}. <br> Also, the user made this comment: ${req.body.message}. <br> User phone: ${req.body.phone}`,
+  };
+  
+  const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  await transport.sendMail(email);
+
+  res.status(201).json({ error: false, message: "Message sent" });
 });
 
 module.exports = router;
