84b4fc80e24b0a515d04800df180ee1f7953860a
Paula Garcia <86069162+pauligarcia8@users.noreply.github.com>
2025-01-29 21:41:40 -0300

    Feature/add environment variable (#13)
    * addtion of environment variables of api route
    
    * Revert "addtion of environment variables of api route"
    
    This reverts commit e3bea06989a6e78cf61906139850c3695857f288.
    
    * addtion of environment variables of api route
    
    * use environment variables

--- frontend/src/pages/ContactPage.js
+++ frontend/src/pages/ContactPage.js
@@ -1,32 +1,73 @@
 import React from "react";
 import "../styles/pages/ContactPage.css";
+import { useState } from "react";
 
 const ContactPage = (props) => {
+  const initialFormData = {
+    name: "",
+    email: "",
+    phone: "",
+    message: "", 
+  }
+
+  const [sending, setSending] = useState(false);
+  const [message, setMessage] = useState("");
+  const [formData, setFormData] = useState(initialFormData);
+
+
+  const handleFormDataChange = (e) => { 
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  const handleSubmit = async (e) => { 
+    e.preventDefault();
+    setMessage("");
+    setSending(true);
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/contact`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await response.json();
+    setSending(false);
+    setMessage(data.message);
+    if (!data.error) {
+      setFormData(initialFormData);
+    }
+  }
+
   return (
     <main className="contact">
       <div>
         <h2>Quick Contact</h2>
-        <form action="" method="" className="form">
+        <form action="/contact" method="post" className="form" onSubmit={handleSubmit}>
           <p>
             <label for="nombre">Name</label>
-            <input type="text" name="" />
+            <input type="text" name="name" value={formData.name} onChange={handleFormDataChange} />
           </p>
           <p>
             <label for="email">Email</label>
-            <input type="text" name="" />
+            <input type="text" name="email" value={formData.email} onChange={handleFormDataChange} />
+
           </p>
           <p>
             <label for="telefono">Phone</label>
-            <input type="text" name="" />
+            <input type="text" name="phone" value={formData.phone} onChange={handleFormDataChange} />
+
           </p>
           <p>
             <label for="mensaje">Message</label>
-            <textarea type="text" name=""></textarea>
+            <textarea type="text" name="message" value={formData.message} onChange={handleFormDataChange}></textarea>
           </p>
           <p className="actions">
             <input type="submit" value="Enviar" />
           </p>
         </form>
+        {sending && <p>Sending...</p>}
+        {message && <p>{message}</p>}
       </div>
       <div className="data">
         <h2>Other means of communication</h2>
